16b024a33951fd88cc64f7b09eaaf75a8291966c
Pavel <pakolozanov@gmail.com>
2021-04-20 21:41:17 +0300

    .

--- WpfGazeApp/MainWindow.xaml.cs
+++ WpfGazeApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Tobii.InteractionLib.Wpf;
 
 namespace WpfGazeApp
 {
@@ -26,5 +14,6 @@ namespace WpfGazeApp
 
             InitializeComponent();
         }
+
     }
 }
