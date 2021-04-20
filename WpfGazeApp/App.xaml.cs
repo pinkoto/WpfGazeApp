2bfcb5b216f05fd3e90a0f6e9d36463fccac69f5
Pavel <pakolozanov@gmail.com>
2021-04-20 20:00:05 +0300

    Added the project

---
+++ WpfGazeApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGazeApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public Tobii.InteractionLib.Wpf.InteractionLibWpfHost IntlibWpf { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e) => IntlibWpf = new Tobii.InteractionLib.Wpf.InteractionLibWpfHost();
+
+        // This function is called when the application exits.
+        protected override void OnExit(ExitEventArgs e)
+        {
+            IntlibWpf?.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
